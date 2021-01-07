f42c2b9b2e8bf8c556be43184523f21601f2b78c
sanketdudhe <sanketd7843@gmail.com>
2021-01-07 22:11:50 +0530

    Initial commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/sanketdudhe/developer/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/sanketdudhe/Downloads/ecommerce-mobile-app-with-admin-panel/Admin and ecommerce/flutter_application"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/sanketdudhe/developer/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.3.1"
+export "FLUTTER_BUILD_NUMBER=1.3.1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
